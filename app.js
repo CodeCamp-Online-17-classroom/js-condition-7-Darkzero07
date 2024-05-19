2376a8901cf95e1559a55f80cbd057ee9930f9ce
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 08:25:57 +0700

    js lab 24

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+let num1 = Number(prompt("Input your number 1 : "));
+let num2 = Number(prompt("Input your number 2 : "));
+let result;
+result = (isNaN(num1) || isNaN(num2)) ? "Invalid number" : (num1+num2);
+alert(result);
